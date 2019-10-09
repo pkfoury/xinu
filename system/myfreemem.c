a2b0e5e7006b88d7f198f6b77b24bec2ed9df814
Peter <pkfoury@purdue.edu>
2019-10-09 16:56:33 -0400

    attempt at working my[free/get]mem

--- system/myfreemem.c
+++ system/myfreemem.c
@@ -6,21 +6,42 @@
  *  myfreemem  -  cache recently freed blocks
  *------------------------------------------------------------------------
  */
-syscall	myfreemem(
-	  char		*blkaddr,	/* Pointer to memory block	*/
-	  uint32	nbytes		/* Size of block in bytes	*/
-	)
-{
-	intmask	mask;			/* Saved interrupt mask		*/
-	struct	memblk	*next, *prev, *block;
-	uint32	top;
+syscall myfreemem(char *blkaddr, /* Pointer to memory block	*/
+                  uint32 nbytes  /* Size of block in bytes	*/
+) {
+  intmask mask; /* Saved interrupt mask		*/
+  mask = disable();
 
-	mask = disable();
-	// error checking
-	
+  struct memblklist *list;
+  struct memblk *freeblock;
+  bool8 fallback = TRUE;
 
+  if ((nbytes == 0) || ((uint32)blkaddr < (uint32)minheap) ||
+      ((uint32)blkaddr > (uint32)maxheap)) {
+    return SYSERR;
+  }
 
+  int i;
+  for (i = 0; i < 20; i++) {
+    list = &cache[i];
+    if (list->blksize == nbytes || list->blksize == 0) {  // if exact list or empty list exists
+      kprintf("inserting into cache\n");
+      if (list->blksize == 0) {
+        list->blksize = nbytes;
+      }
+      fallback = FALSE;
+      freeblock = (struct memblk *)blkaddr;
+      freeblock->mnext = list->head;
+      list->head = freeblock;
+      break;
+    }
+  }
 
-	restore(mask);
-	return OK;
+  // if cache is full, fallback to freemem()
+  if (fallback) {
+    freemem(blkaddr, nbytes);
+  }
+
+  restore(mask);
+  return OK;
 }
--- system/mygetmem.c
+++ system/mygetmem.c
@@ -6,10 +6,45 @@
  *  mygetmem  -  get memory blocks from cache
  *------------------------------------------------------------------------
  */
-char *mygetmem(
-	uint32 nbytes // size of memory to get
-)
-{
+char *mygetmem(uint32 nbytes  // size of memory to get
+) {
+  intmask mask; /* Saved interrupt mask		*/
+  mask = disable();
 
-	return;
+  struct memblklist *list;
+  struct memblk *block;
+
+  int i;
+  for (i = 0; i < 20; i++) {
+    list = &cache[i];
+    if (list->blksize == nbytes) {  // if list exists, get from list
+      kprintf("list found\n");
+      block = list->head;
+      list->head = block->mnext;
+      block->mnext = (struct memblk *)NULL;
+      restore(mask);
+      return (char *)block;
+    }
+  }
+
+  // if not in cache, fallback to getmem()
+  char *mem = getmem(nbytes);
+
+  // if getmem() == SYSERR, clear cache and try again.
+  if (mem == (char *)SYSERR) {
+    kprintf("getmem SYSERR\n");
+    for (i = 0; i < 20; i++) {
+      list = &cache[i];
+      list->blksize = 0;
+      block = list->head;
+      while (block) {
+        freemem((char *)block, block->mlength);
+        block = block->mnext;
+      }
+    }
+    mem = getmem(nbytes);
+  }
+
+  restore(mask);
+  return mem;
 }
