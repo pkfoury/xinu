386bc8c7a98fc17948cc6117ad0b03e0c8f2e8bc
Peter <pkfoury@purdue.edu>
2019-11-17 20:32:03 -0500

    completed part1

--- system/main.c
+++ system/main.c
@@ -2,22 +2,78 @@
 
 #include <xinu.h>
 
-process	main(void)
+/*------------------------------------------------------------------------
+ * udp_echo  -  echo message sent over UDP
+ *------------------------------------------------------------------------
+ */
+uint16 udp_echo()
+{
+	uint32 remoteip = dnslookup("xinu14.cs.purdue.edu");
+	uint16 remoteport = 44000;
+	uint16 localport = 42069;
+
+	uid32 slot = udp_register(remoteip, remoteport, localport);
+	if (slot == SYSERR) {
+		return SYSERR;
+	}
+	
+	char *buff = "udp echo test";
+	udp_send(slot, buff, 14);
+
+	char *response = "";
+	int32 responselength = 0;
+	if( (responselength = udp_recv(slot, response, 14, 500)) == TIMEOUT) {
+		kprintf("timeout, running attempt 2\n");
+		udp_send(slot, buff, 14);
+		if(udp_recv(slot, response, 14, 500) == TIMEOUT) {
+			kprintf("timeout\n");
+			return SYSERR;
+		}
+	}
+	if(responselength != 14 || strcmp(buff, response) != 0) {
+		kprintf("bad response\n");
+		kprintf("buff: %s\n", buff);
+		kprintf("response: %s\n", response);
+		kprintf("strcmp: %d\n", strcmp(buff, response));
+		return SYSERR;
+	}
+	kprintf("success\n");
+	return OK;
+}
+
+/*------------------------------------------------------------------------
+ * chatroom  -  
+ *------------------------------------------------------------------------
+ */
+uint16 chatroom()
 {
 
-	/* Run the Xinu shell */
-
-	recvclr();
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	while (TRUE) {
-		receive();
-		sleepms(200);
-		kprintf("\n\nMain process recreating shell\n\n");
-		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
-	}
 	return OK;
-    
+}
+
+
+process main(void)
+{
+	recvclr();
+
+	kprintf("input 1 for udp_echo or 2 for chat room.\n");
+	char *input = getmem(2);
+	read(CONSOLE, input, 2);
+
+	if (input[0] == '1') {
+		kprintf("running udp_echo\n");
+		udp_echo();
+	}
+	else if (input[0] == '2') {
+		kprintf("running udp_echo\n");
+		chatroom();
+	}
+	else {
+		kprintf("invalid; running shell\n");
+		while (TRUE) {
+			resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
+		}
+	}
+
+	return OK;
 }
